82c7be14f07c2388507f9a2f0488a2c1dc9650b5
mtt-merz <mtt.merz@gmail.com>
2024-11-10 21:34:42 +0100

    refactor: add theme folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
+import "@/theme/globals.css";
+
 export const metadata = {
-  title: 'mmmukkey'
-}
+  title: "mmmukkey",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
