6ead6ea92c8c3ac12695c2d8ae3172cec247bbee
mtt-merz <mtt.merz@gmail.com>
2024-11-18 19:23:01 +0100

    feat: add custom fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { PropsWithChildren } from "react";
 import { cookies } from "next/headers";
 import { getThemeFromCookies } from "@/utils/theme";
 import App from "@/components/App";
+import { gluten, poppins } from "@/app/fonts";
 
 export const metadata = {
   title: "mukkey",
@@ -12,7 +13,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
   const theme = getThemeFromCookies(await cookies());
 
   return (
-    <html lang="en">
+    <html lang="en" className={`${poppins.variable} ${gluten.variable}`}>
       <body data-theme={theme}>
         <App>{children}</App>
       </body>
